10733990cb9ea96974031251aa683ed7acc4a5c0
mizushima1226 <mizuki.bz.3224@gmail.com>
2020-04-18 14:51:33 +0900

    install optimize-css-assets-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 
 const outputPath = path.resolve(__dirname, 'dist');
 
@@ -68,7 +70,8 @@ module.exports = {
                         drop_console: true
                     }
                 }
-            })
+            }),
+            new OptimizeCSSAssetsPlugin({})
         ],
     },
 }
